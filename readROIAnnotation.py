996ac0b37dbf7e0e516feaf8d77d84dc11f7fe7b
YulinS-UoB <ok18185@bristol.ac.uk>
2022-07-05 14:16:54 +0100

    0705

--- readROIAnnotation.py
+++ readROIAnnotation.py
@@ -11,6 +11,9 @@ import matplotlib.pyplot as plt
 
 class AnnotationInterprator:
 
-    def __init__(self):
-        pass
-    
+    def __init__(self, annotation_list, image_type='uint16'):
+        self.pathFGAnno = annotation_list[0]
+        self.pathBGAnno = annotation_list[1]
+        self.imageStack = annotation_list[2]
+        self.imgType = image_type
+    def 
