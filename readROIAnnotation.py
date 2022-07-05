06bc47f3ac9fe6473a9b2997af8dd221c1bde11e
YulinS-UoB <ok18185@bristol.ac.uk>
2022-07-05 14:04:01 +0100

    Formal Update 0705

---
+++ readROIAnnotation.py
@@ -0,0 +1,16 @@
+# ***********************************
+import cv2 as cv
+import numpy as np
+import os
+from roifile import ImagejRoi
+import skimage.io as skio
+from skimage.draw import polygon
+import matplotlib.pyplot as plt
+# ************************************
+
+
+class AnnotationInterprator:
+
+    def __init__(self):
+        pass
+    
