79a3fa9277559ea9359d38d5b6aa63aca5433a46
YulinS-UoB <ok18185@bristol.ac.uk>
2022-07-06 20:12:15 +0100

    Update generateTrainingSet.py

--- generateTrainingSet.py
+++ generateTrainingSet.py
@@ -8,4 +8,5 @@ class TrainingSetGenerator:
 
     def __init__(self, annotation_meta):
         self.labelMap = annotation_meta['label map']
-        
+        self.imgStack = np.load('{}.npy'.format(annotation_meta['image saving prefix']))
+        self.maskStack = np.load('{}.npy'.format(annotation_meta['mask saving prefix']))
