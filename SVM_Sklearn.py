b62aa3bc5b27c0262c046b24aa11dd0a50940b7d
YulinS-UoB <ok18185@bristol.ac.uk>
2022-08-09 21:49:10 +0100

    aa

--- SVM_Sklearn.py
+++ SVM_Sklearn.py
@@ -1,6 +1,6 @@
 # ***********************************
 from sklearn import svm
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.neural_network import MLPClassifier
@@ -12,21 +12,22 @@ import datetime
 import cv2 as cv
 import os
 # ***********************************
-DataRoot = 'data/AnnotationDS2022-08-04_17_32/'
-DataBar = 5
+DataRoot = 'data/AnnotationDS2022-08-09_21_39/'
 
 
-def generateDSet(data_bar, exclude=None, data_root=DataRoot, seeding='Auto'):
+def generateDSet(data_bar, data_root=DataRoot, exclude=None, seeding=None, batch_size=800):
     # seeding can either be 'Auto' as default or a number which makes random dataset reproducable
     # format of exclude: (seed, exclude size)
-    if seeding == 'Auto':
+    if seeding is None:
         seed1 = np.random.randint(3000)
         print('The seed for reading file is decided as: ', seed1)
         seed2 = np.random.randint(3000)
         print('The seed for shuffling data is decided as: ', seed2)
     else:
         seed1 = seeding[0]
+        print('The seed for reading file is input as: ', seed1)
         seed2 = seeding[1]
+        print('The seed for shuffling data is input as: ', seed2)
     neg_data_list = os.listdir(data_root + '001/')
     neg_data_list.sort()
     pos_data_list = os.listdir(data_root + '002/')
@@ -39,7 +40,7 @@ def generateDSet(data_bar, exclude=None, data_root=DataRoot, seeding='Auto'):
             niet_neg = np.random.choice(list(range(neg_data_num)), size=exclude[1])
             niet_pos = np.random.choice(list(range(pos_data_num)), size=exclude[1])
             list_left_neg = np.setdiff1d(list(range(neg_data_num)), niet_neg)
-            list_left_pos = np.setdiff1d(list(range(pos_data_num)), niet_neg)
+            list_left_pos = np.setdiff1d(list(range(pos_data_num)), niet_pos)
         else:
             list_left_neg = np.asarray(range(neg_data_num))
             list_left_pos = np.asarray(range(pos_data_num))
@@ -58,46 +59,53 @@ def generateDSet(data_bar, exclude=None, data_root=DataRoot, seeding='Auto'):
     data_list = []
     label_list = []
     for i in itrtr:
-        data_list.append(np.load("{}001/{}" + neg_data_list[itrtr_neg[i]]))
-        label_list.append(np.ones((data_list[0].shape[0])))
-        data_list.append(np.load("{}002/{}" + pos_data_list[itrtr_pos[i]]))
-        label_list.append(2 * np.ones((data_list[0].shape[0])))
+        data_list.append(np.load("{}001/{}".format(data_root, neg_data_list[itrtr_neg[i]])))
+        label_list.append(np.ones((data_list[-1].shape[0])))
+        data_list.append(np.load("{}002/{}".format(data_root, pos_data_list[itrtr_pos[i]])))
+        label_list.append(2 * np.ones((data_list[-1].shape[0])))
     test_data = np.concatenate(data_list).astype('int')
     test_label = np.concatenate(label_list).astype('int')
     np.random.seed(seed2)
     np.random.shuffle(test_data)
     np.random.seed(seed2)
     np.random.shuffle(test_label)
+    test_data = test_data[0:test_data.shape[0] // (batch_size * 10) * (batch_size * 10), :]
+    test_label = test_label[0:test_label.shape[0] // (batch_size * 10) * (batch_size * 10)] - 1
 
     return test_data, test_label
 
 
-x, y = generateDSet(data_bar=[0, 1, 2, 3, 4])
+x_test, y_test = generateDSet(data_bar=5, seeding=(1428, 2500))
+
+x_val, y_val = generateDSet(data_bar=2, exclude=(1428, 5))
+
 
 model_tf = tf.keras.Sequential([
     tf.keras.Input(shape=(48,), batch_size=800),
+    tf.keras.layers.Dense(units=24, activation='sigmoid'),
+    tf.keras.layers.Dense(units=12, activation='sigmoid'),
     tf.keras.layers.Dense(units=1, activation='sigmoid'),
 
 ])
 model_tf.compile(
     optimizer='adam',
-    loss='hinge',
-    metrics=['categorical_accuracy']
+    loss='mean_absolute_error',
+    metrics=['mean_absolute_error']
 )
 
-clf3 = MLPClassifier(batch_size=800, activation='logistic', hidden_layer_sizes=(24, 12, 4, 1), random_state=1428,
-                     verbose=True, n_iter_no_change=1500, max_iter=1500)
+
+model_tf.fit(x_test, y_test, batch_size=800, epochs=100)
 
 
-x2, y2 = generateDSet(data_bar='T00000B001.npy')
-x3, y3 = generateDSet(data_bar='T00000B002.npy')
-x4, y4 = generateDSet(data_bar='T00000B003.npy')
-x5, y5 = generateDSet(data_bar='T00000B004.npy')
-clf3.fit(x, y)
-clf3.fit(x2, y2)
-clf3.fit(x3, y3)
-clf3.fit(x4, y4)
-y5H = clf3.predict(x5)
+clf2 = RandomForestRegressor(n_estimators=1000, n_jobs=5, verbose=1)
+
+clf3 = MLPClassifier(batch_size=800, activation='logistic', hidden_layer_sizes=(48, 24, 12, 6, 1), random_state=1428,
+                     verbose=True, n_iter_no_change=50, max_iter=50)
+clf2.fit(x_test, y_test)
+clf3.fit(x_test, y_test)
+
+y_valH2 = clf2.predict(x_val)
+y_valH = clf3.predict(x_val)
 
 testImg = np.load('data/AnnotationDS2022-08-04_17_32/Processed_Stack_T00000.npy')
 testX = testImg[:, :, :, 0].reshape((48, 2160 * 2560))
@@ -109,8 +117,22 @@ testYNDA = testY.reshape((2160, 2560))
 plt.imshow(testYNDA)
 plt.colorbar()
 
-testYNDA_Blurred = filters.gaussian(testYNDA, sigma=3, preserve_range=True)
-testYNDA_Blurred[testYNDA_Blurred < 1.25] = 0
+t1 = time.time()
+testY2 = clf2.predict(testX)
+print('Time consumed: {} secs'.format(time.time()-t1))
+testYNDA2 = testY2.reshape((2160, 2560))
+plt.imshow(testYNDA2)
+plt.colorbar()
+
+t1 = time.time()
+testY2 = model_tf.predict(testX, batch_size=800)
+print('Time consumed: {} secs'.format(time.time()-t1))
+testYNDA2 = testY2.reshape((2160, 2560))
+plt.imshow(testYNDA2)
+plt.colorbar()
+
+testYNDA_Blurred = filters.gaussian(testYNDA, sigma=2, preserve_range=True)
+testYNDA_Blurred[testYNDA_Blurred < 0.5] = 0
 testYNDA_Blurred[testYNDA_Blurred > 0] = 1
 plt.imshow(testYNDA_Blurred)
 plt.colorbar()
@@ -119,4 +141,3 @@ with open('{}model_{}.pkl'.format(DataRoot, datetime.datetime.now().strftime('%Y
     pickle.dump(clf3, pkf)
 
 cv.imwrite('{}FinalImgPrdct.tiff'.format(DataRoot), (testYNDA_Blurred * 32767).astype('uint16'))
-
